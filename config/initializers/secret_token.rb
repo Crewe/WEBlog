51edc581d3bd604590899a61afaf339d15e5c1b0
Crewe <github@ryanedwardscrewe.com>
2012-03-29 23:29:06 -0400

    forgot to add these :/

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WEBlog::Application.config.secret_token = 'e85eb8518cc3a616da4efa4b720b6ba674738a91cd15d130747af2c443746501d5544119aba250de6fc39e8856169e32c887d4dcc8fd7eff2471bcec17973f60'
